caf4bd57f9ca2a41ce63feb06553a3fb9ae3fa75
Calvin Boyce <calvinrboyce@gmail.com>
2019-04-19 10:33:55 -0600

    Nylas update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
+const Nylas = require('nylas');
 
 //initialization
 const app = express();
@@ -14,14 +15,21 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
+//nylas setup
+Nylas.config({
+    appId: '9lkzcjkmcbkbmndwi0wd9tnhe',
+    appSecret: '2dq26yihbncaj9wg5b2bws92n',
+});
+
+
 //root URL GET
 app.get('/', function(req, res) {
-    res.render('main.ejs', {port:port});
+    res.render('main.ejs', {port});
 });
 
 //userCreate URL GET (sign up)
 app.get('/userCreate', function(req, res) {
-    res.render('userCreate.ejs', {port:port});
+    res.render('userCreate.ejs', {port});
 });
 
 //userCreate/new URL POST
@@ -40,7 +48,7 @@ app.post('/userCreate/new', function(req, res) {
 
 //user URL GET (sign in)
 app.get('/user', function(req, res) {
-    res.render('userValidate.ejs', {port:port});
+    res.render('userValidate.ejs', {port});
 });
 
 //user/validate URL POST
@@ -50,7 +58,26 @@ app.post('/user/validate', function(req, res) {
 
 //user/id URL GET (settings)
 app.get('/user/:id', function(req, res) {
-    res.render('userSettings.ejs', {username:req.params.id, port:port});
+    res.render('userSettings.ejs', {username:req.params.id, port});
+});
+
+//connect to nylas oauth
+app.get('/connect', (req, res, next) => {
+    options = {
+        redirectURI: 'http://localhost:' + port + '/oauth/callback',
+    };
+    res.redirect(Nylas.urlForAuthentication(options));
+});
+
+//handle response
+app.get('/oauth/callback', (req, res, next) => {
+    if (req.query.code) {
+        Nylas.exchangeCodeForToken(req.query.code).then(token => {
+            //save code
+        })
+    } else if (req.query.error) {
+        res.render('oauthError.ejs', {error:req.query.reason, port});
+    }
 });
 
 //running confirmation message
